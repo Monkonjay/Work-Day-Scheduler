fec713d7bb899feceab06edcf04e398007e464e4
R Greene <greenbert81@gmail.com>
2022-07-25 08:54:33 -0700

    formatting moments

--- Develop/script.js
+++ Develop/script.js
@@ -1,15 +1,17 @@
 var current_day = $('#currentDay');
 current_day.text(moment().format("ddd MMM Do"));
 var saveNineBtn = $('#save_nine');
+let containerEl = $('.container');
+let nineBlock = $('#9');
 
 
-var nine = $('#9');
-// localStorage.setItem('9', 'test value');
+// var nine = $('#9');
+// localStorage.setItem('nine_message', 'test value');
 // $(nine).val(localStorage.getItem('9'));
 
 
-var nine_message = localStorage.getItem('nine_message');
-$(nine).val = nine_message;
+// var nine_message = localStorage.getItem('nine_message');
+// $(nine).val = nine_message;
 
 
 //var save9 = $('#save9');
@@ -17,10 +19,41 @@ console.log($('#save_nine'))
 
 saveNineBtn.on('click', function() {
     localStorage.setItem('nine_message', nine_message);
-    console.log("Im hit");
+    console.log("I'm hit");
 });
 
-console.log(moment().format('h'));
+// console.log(moment().format('hA') === '7PM');
+// console.log(nineBlock.attr('data-hour'));
+// console.log(nineBlock.attr('data-hour') > moment().format('hA'));
+console.log('12PM' > moment().format('hA'));
+
+
+if (nineBlock.attr('data-hour') > moment().format('hA')) {
+  nineBlock.addClass('future');
+
+} else if (nineBlock.attr('data-hour') < moment().format('hA')) {
+  nineBlock.addClass('past');
+
+} else {
+  nineBlock.addClass('present');
+};
+
+
+
+// console.log(containerEl.children[0].children[0].text);
+// console.log(containerEl.children().eq(0).attr('data-pm'));
+
+// rootEl.children('ul').children().eq(6).text('O');
+
+
+// for(i = 0; i < containerEl.children().length; i++) {
+//   console.log('data-hour'[i]);
+// console.log(containerEl.children('div').children().eq(1).attr('data-hour'));
+// };
+
+
+
+
 
 
 // var count = localStorage.getItem("count");
