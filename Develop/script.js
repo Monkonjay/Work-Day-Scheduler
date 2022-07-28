fe16651656a49757a8bea539cef7c4e74301aa49
R Greene <greenbert81@gmail.com>
2022-07-27 18:41:59 -0700

    local storage .setItem setup without loop

--- Develop/script.js
+++ Develop/script.js
@@ -1,66 +1,43 @@
-var current_day = $('#currentDay');
-current_day.text(moment().format("ddd MMM Do"));
-let currentTime = moment().hour();
-var saveBtn = $('#saveBtn');
-let containerEl = $('.container');
-let nineBlock = $('#9');
+$(function () {
+  // global variables
+  const current_day = $('#currentDay');
+  const saveBtn = $('.saveBtn');
+ 
+  
+  const todos = $('.col-1.hour').each(() => {
+    console.log($(this).text());
+  });
+  console.log(todos);
+  // format date and time using moments.js
+  current_day.text(moment().format("ddd MMM Do"));
+  const currentTime = moment().hour();
+
+  // dynamically add classes to textarea to change background color
+  $("textarea").each(function () {
+    if ($(this).attr('data-hour') > currentTime) {
+      $(this).addClass('future');
+    } else if ($(this).attr('data-hour') < currentTime) {
+      $(this).addClass('past');
+    } else {
+      $(this).addClass('present');
+    }
+  });
+
+  // testing data attribute of save buttons
+  // $("#saveBtn").each(function() {
+  //   console.log($(this).attr('data-save')); 
+  // });
 
 
-// var nine = $('#9');
-// localStorage.setItem('nine_message', 'test value');
-// $(nine).val(localStorage.getItem('9'));
 
-
-var btn_message = localStorage.getItem('message');
-// $(nine).val = nine_message;
-
-
-//var save9 = $('#save9');
-console.log($('#saveBtn'))
-
-saveBtn.on('click', function() {
-    localStorage.setItem('message', btn_message);
+  // add event listener to save buttons
+  saveBtn.on('click', function () {
+    const todo = $('#9').val();
+    console.log('TODO', todo);
+    localStorage.setItem('todo', todo);
+    // todo = $('#9').val;
     console.log("I'm hit");
+  });
+
+
 });
-
-
-// console.log(curentTime > nineBlock.attr('data-hour'));
-
-$("textarea").each(function() {
-  if ($(this).attr('data-hour') > currentTime) {
-    $(this).addClass('future');
-  } else if ($(this).attr('data-hour') < currentTime) {
-    $(this).addClass('past');
-  } else {
-    $(this).addClass('present');
-  }
- });
-
-
-// $("textarea").addClass('present');
-
-// $("textarea").each(function() {
-//   console.log($(this).attr('data-hour'))
-//  })
-
-
-
-// var count = localStorage.getItem("count");
-
-// counter.textContent = count;
-
-// addButton.addEventListener("click", function() {
-//   if (count < 24) {
-//     count++;
-//     counter.textContent = count;
-//     localStorage.setItem("count", count);
-//   }
-// });
-
-// subtractButton.addEventListener("click", function() {
-//   if (count > 0) {
-//     count--;
-//     counter.textContent = count;
-//     localStorage.setItem("count", count);
-//   }
-// });
